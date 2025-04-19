7fe1df72427ba8bedf438aabb3a8a6d66d7fde5a
workrom.24 <workrom24@gmail.com>
2025-04-19 16:37:05 +0300

    Add project files.

---
+++ CPU.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab05Lib
+{
+    public class CPU
+    {
+        public string Model;
+        public string Manufacturer;
+        public int Cores;
+        public int Threads;
+        public double Frequency;
+        public int CacheMB;
+        public int TDPWatts;
+        public bool HasHyperThreading;
+
+        public double Performance
+        {
+            get
+            {
+                return CalculatePerformance();
+            }
+        }
+        public double CalculatePerformance()
+        {
+            return Threads * Frequency;
+        }
+    }
+}
